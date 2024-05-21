b83098ddd587771b7942513c4b3a20e35978d19f
Bippity <bippity@users.noreply.github.com>
2024-05-21 09:34:41 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		// Replace 1st argument with pathway to ModOrganizer.
+		Process.Start(@"B:\Games\Wabbajack\modlists\eldergleam\ModOrganizer.exe", @"moshortcut://:SKSE");
+	}
+}
